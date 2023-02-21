f65332e2595c7f7ce0ecdeacb663dda9a35f9066
qwer1234abc <kelvinbehcy@gmail.com>
2023-02-21 15:59:35 +0800

    Static and Global Recursion done

--- Data Structures/Data Structures.cpp
+++ Data Structures/Data Structures.cpp
@@ -1,7 +1,56 @@
+// Data structure is the arrangement of collection data items so that operations on them can be done efficiently.
+
 #include <iostream>
+using namespace std;
+
+
+// Recursion happens when a function calls it self can be for ascending and descending and musts have a base condition or a infinite loop will occur
+void BasicRecursion(int n)
+{
+	if (n > 0)
+	{
+		cout << n << endl;
+		BasicRecursion(n - 1);
+	}
+}
+// static and global variables in Recursion:
+// int x = 0 (global variable) same result as static
+int staticRecursion(int n)
+{
+	// x here will only have a single copy unlike n, x will be n here for every recursion
+	static int x = 0;
+	if (n > 0)
+	{
+		x++;
+		return staticRecursion(n - 1) + x;
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	BasicRecursion(3);
+
+	int r = staticRecursion(3);
+	cout << r;
+
+	r = staticRecursion(3);
+	cout << r;
+
+
+
+
+
+
+    return 0;
 }
 
